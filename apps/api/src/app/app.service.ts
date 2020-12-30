a954f183987ed4eed6021823843d356d0ccadfbb
Maliiiko <malikcatic@googlemail.com>
2020-12-30 19:42:48 +0100

    update message

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Message } from '@mreza-mladih-platform/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to api!' };
+    return { message: 'Hello Malik!' };
   }
 }
