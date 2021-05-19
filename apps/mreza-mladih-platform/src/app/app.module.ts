223f35a9c844c6898e4b098cf931707b5106f5b4
Maliiiko <malikcatic@googlemail.com>
2021-05-19 21:19:02 +0200

    FIRST COMMIT FOR FOOTER COMPONENt

--- apps/mreza-mladih-platform/src/app/app.module.ts
+++ apps/mreza-mladih-platform/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
