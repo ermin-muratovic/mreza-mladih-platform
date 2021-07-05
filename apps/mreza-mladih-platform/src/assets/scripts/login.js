0442a0e0bd93b0d2387572f586f41c0ce165eaba
Maido47 <maid768@gmail.com>
2021-07-05 11:32:22 +0200

    Add Register/Login Form

---
+++ apps/mreza-mladih-platform/src/assets/scripts/login.js
@@ -0,0 +1,17 @@
+// LOG IN UND SIGN UP PAGE JAVA SCRIPT
+window.onload=function () {
+  const sign_in_btn = document.getElementById("sign-in-btn");
+  const sign_up_btn = document.getElementById("sign-up-btn");
+  const container = document.querySelector(".containerLOGSIGN");
+
+  sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+  });
+
+
+  sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+  });
+}
+
+
