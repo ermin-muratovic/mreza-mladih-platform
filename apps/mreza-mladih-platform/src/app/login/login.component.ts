9c4ba900fe27c7d74ef6a5949f119122bcdbba39
Maido47 <maid768@gmail.com>
2021-07-06 12:45:30 +0200

    Add Click Listener for Login Toggle class

--- apps/mreza-mladih-platform/src/app/login/login.component.ts
+++ apps/mreza-mladih-platform/src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  mode = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(): void {
+    this.mode = !this.mode;
   }
 
 }
